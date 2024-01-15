c49435516060c1835f189b4db3fb38dd5eefd362
IgorLTS10 <ichrzyptowicz@myges.fr>
2024-01-15 15:26:33 +0100

    Navbar - magnifying glass

---
+++ deswap/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+// Navbar.tsx
+import React from 'react';
+import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar">
+      <div className="left-section">
+        <span className="logo">DeSwap</span>
+        <div className="nav-links">
+          <span>Swap</span>
+          <span>Tokens</span>
+          <span>Pool</span>
+        </div>
+      </div>
+      <div className="right-section">
+        <div className="search-container">
+          <FontAwesomeIcon icon={faSearch} className="search-icon" />
+          <input type="text" placeholder="Search tokens" />
+        </div>
+      </div>
+      <div className="connection-button">
+        <button>Connect </button>
+        <div className="logo-square"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

28f28f666e09f7fb69dad916392e0673804f7942
AgustinGomezDelToro <66341782+AgustinGomezDelToro@users.noreply.github.com>
2024-01-16 14:57:37 +0100

    creation demo page tokens

--- deswap/src/components/Tokens.tsx
+++ deswap/src/components/Tokens.tsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 function Tokens() {
     return (
+        <>
         <div>Tokens</div>
+        <div>Tokens</div>
+        <div>
+            <form>
+                <label>
+                    Token address:
+                    <input type="text" name="tokenAddressAdd" />
+                </label>
+                <input type="submit" value="Add Token" />
+            </form>
+        </div>
+        <div>
+            <form>
+                <label>
+                    Token address:
+                    <input type="text" name="tokenAddressDelete" />
+                </label>
+                <input type="submit" value="Delete Token" />
+            </form>
+        </div>
+        </>
     )
 }
 
